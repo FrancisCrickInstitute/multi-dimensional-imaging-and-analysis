c1bde89e29c6be773381b846ca743279f7601823
David Barry <david.barry@crick.ac.uk>
2024-08-09 16:56:04 +0100

    Added compare_track_data.ipynb

--- compare_tracks.py
+++ compare_tracks.py
@@ -8,8 +8,6 @@ file_path_7 = 'Z:/working/Training/2024-Crick Microscopy Course/Live imaging/tri
 data7 = pd.read_csv(file_path_7, skiprows=[1, 2, 3])
 data7['POSITION'] = 7
 
-speed_data = pd.concat([data0[['TRACK_MEAN_SPEED', 'POSITION']], data7[['TRACK_MEAN_SPEED', 'POSITION']]])
-
 plt.figure(figsize=(6.0, 5.0), dpi=300)
 plt.hist(data0['TRACK_MEAN_SPEED'], bins=20, alpha=0.7, label='Pos0', histtype='barstacked')
 plt.hist(data7['TRACK_MEAN_SPEED'], bins=20, alpha=0.7, label='Pos7', histtype='barstacked')
